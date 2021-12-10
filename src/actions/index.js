b868dce6c40500432c26b3a5ded63252bc0dd156
Lia Moua <moua0061@yahoo.com>
2021-12-09 22:43:18 -0500

    craeted my actions and consts

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,6 +4,18 @@ export const FETCH_START = 'FETCH_START';
 export const FETCH_SUCCESS = 'FETCH_SUCCESS';
 export const FETCH_FAIL = 'FETCH_FAIL';
 
+export const fetchStart = () => {
+    return({type:FETCH_START})
+}
+
+export const fetchSuccess = (smurf) => {
+    return({type:FETCH_SUCCESS, payload: smurf})
+}
+
+export const fetchFail = (error) => {
+    return({type:FETCH_FAIL, payload: error})
+}
+
 
 
 //Task List:
